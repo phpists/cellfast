75ac54986763b9f53d6269613df5bc9dd6e2f492
Nazarts <naziksprey.2000@gmail.com>
2023-07-28 15:53:50 +0300

    fix issues

--- cellfast/views/site/search/articles/index-articles.php
+++ cellfast/views/site/search/articles/index-articles.php
@@ -1,15 +1,20 @@
+<?php
 
+\cellfast\assets\ContentsAsset::register($this);
+
+/**
+ * Контейнер вывода списка элементов (используется в статья и новостях как общий шаблон).
+ */
+
+/* @var $this yii\web\View */
+/* @var $dataProvider \yii\data\ActiveDataProvider */
+?>
 <section class="content <?= $__params['id'] ?>-index">
     <div class="container">
 
-        <?= \cellfast\widgets\Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            'options' => [],
-        ]) ?>
-
         <?= \cellfast\widgets\Alert::widget() ?>
 
-        <div class="content__title"><span><?= $this->title ?></span></div>
+        <div class="content__title"><span><?= Yii::t('app', 'Articles') ?></span></div>
 
         <div id="<?= $__params['items-wrapper'] ?>" class="content__body <?= $__params['id'] ?>-items">
             <?= $this->render('_items', [
