7192c9a2051628ec09730a008d9ad4132a05d07d
Ivan <ddafihh@gmail.com>
2023-07-28 15:48:17 +0300

    search

--- cellfast/views/site/search/articles/_items.php
+++ cellfast/views/site/search/articles/_items.php
@@ -9,16 +9,10 @@
 /* @var $__params string[] */
 
 ?>
-<?php if (!$articles->count) : ?>
-    <div class="empty-result">
-        <?= $__params['empty']?>
-    </div>
-<?php else : ?>
-    <?php foreach $articles as $i => $model) : ?>
+    <?php foreach ($articles as $i => $model) : ?>
         <?= $this->render('_item', [
             'model' => $model,
             'row_index' => $i,
             '__params' => $__params,
         ])?>
     <?php endforeach ?>
-<?php endif ?>
